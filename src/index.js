72ef88fd1aabc931e2168ee20a92c601cffa8c60
shukhrat <shukhratgadoev@gmail.com>
2019-11-19 09:33:55 +0500

    feat: Initial Commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./style.css";
+
+const App = () => {
+  return <div className="welcome">Hello React,Webpack4</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
